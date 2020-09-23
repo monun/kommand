cbbc0075d0c277c849f6140a1a62d04894a607c4
Noonmaru <noonmaru10@gmail.com>
2020-09-24 05:56:08 +0900

    Add BooleanArgument.kt

---
+++ src/main/kotlin/com/github/noonmaru/kommand/argument/BooleanArgument.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Noonmaru
+ *
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/gpl-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.noonmaru.kommand.argument
+
+import com.github.noonmaru.kommand.KommandContext
+
+object BooleanArgument : KommandArgument<Boolean> {
+    override val parseFailMessage: String
+        get() = "${KommandArgument.TOKEN} <-- true 혹은 false를 입력하세요."
+
+    override fun parse(context: KommandContext, param: String): Boolean {
+        return param.toBoolean()
+    }
+
+    override fun listSuggestion(context: KommandContext, target: String): Collection<String> {
+        return listOf("true", "false")
+    }
+}
