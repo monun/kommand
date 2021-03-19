43cba028f82591c98223ebf2e6ebf219ec334b43
monun <monun1010@gmail.com>
2021-03-20 00:50:00 +0900

    :hammer: SCIPRT: JUnit core를 RuntimeOnly에서 Implementation으로 변경
    이유는 모르겠음
    Test가 안되다가 Runtime으로 하니 되고
    다시 Implementation으로 바꾸니 또 됨
    버근가

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     testImplementation("org.mockito:mockito-core:3.6.28")
     testImplementation("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
 }
