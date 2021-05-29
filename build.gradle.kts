8a399e0025e305c79c74f58e55a7caee298a11ed
monun <monun1010@gmail.com>
2021-05-29 20:54:44 +0900

    :arrow_down: 롤백

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.4.32"
     `maven-publish`
 }
 
@@ -15,12 +15,12 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     testImplementation("org.mockito:mockito-core:3.6.28")
-    testImplementation("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
+    testImplementation("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
 }
 
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "16"
+        kotlinOptions.jvmTarget = "11"
     }
     create<Jar>("sourcesJar") {
         from(sourceSets["main"].allSource)
