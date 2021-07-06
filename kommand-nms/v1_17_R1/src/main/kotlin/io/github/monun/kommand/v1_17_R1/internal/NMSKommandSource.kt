98185401b387582ab6f3b4ca76edfe020ff684aa
Monun <monun1010@gmail.com>
2021-07-06 22:22:26 +0900

    CLEAN UP

--- kommand-nms/v1_17_R1/src/main/kotlin/io/github/monun/kommand/v1_17_R1/internal/NMSKommandSource.kt
+++ kommand-nms/v1_17_R1/src/main/kotlin/io/github/monun/kommand/v1_17_R1/internal/NMSKommandSource.kt
@@ -13,16 +13,22 @@ class NMSKommandSource(
 ) : KommandSource {
     override val sender: CommandSender
         get() = nms.bukkitSender
+
     override val entity: Entity
         get() = nms.entityOrException.bukkitEntity
+
     override val entityOrNull: Entity?
         get() = nms.entity?.bukkitEntity
+
     override val player: Player
         get() = nms.playerOrException.bukkitEntity
+
     override val playerOrNull: Player?
         get() = nms.entity?.bukkitEntity?.takeIf { it is Player } as Player?
+
     override val position: Position
         get() = nms.position.run { Position(x, y, z) }
+
     override val rotation: Rotation
         get() = nms.rotation.run { Rotation(x, y) }
 }
