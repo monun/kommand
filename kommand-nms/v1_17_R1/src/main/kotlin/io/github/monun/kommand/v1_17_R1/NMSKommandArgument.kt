32fbff776af37c79799798b062303f3deba84f35
Monun <monun1010@gmail.com>
2021-07-07 06:08:18 +0900

    :poop: 작업하기 용이하게 같은 파일로 이동

--- kommand-nms/v1_17_R1/src/main/kotlin/io/github/monun/kommand/v1_17_R1/NMSKommandArgument.kt
+++ kommand-nms/v1_17_R1/src/main/kotlin/io/github/monun/kommand/v1_17_R1/NMSKommandArgument.kt
@@ -1,8 +1,11 @@
 package io.github.monun.kommand.v1_17_R1
 
-import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.arguments.*
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import io.github.monun.kommand.KommandArgument
+import io.github.monun.kommand.KommandArgumentSupport
+import io.github.monun.kommand.StringType
 import io.github.monun.kommand.internal.AbstractKommandArgument
 import net.minecraft.commands.CommandSourceStack
 import java.lang.reflect.Method
@@ -41,5 +44,40 @@ open class NMSKommandArgument<T, U>(
     fun from(context: CommandContext<CommandSourceStack>, name: String): U {
         val value = provider(context, name)
         return converter(value)
+    }
+}
+
+private infix fun <T, U> Pair<ArgumentType<T>, (CommandContext<CommandSourceStack>, name: String) -> T>.convert(
+    converter: (T) -> U
+) = NMSKommandArgument(first, second, converter)
+
+
+class NMSKommandArgumentSupport : KommandArgumentSupport {
+    override fun bool(): KommandArgument<Boolean> {
+        return BoolArgumentType.bool() to BoolArgumentType::getBool convert { it }
+    }
+
+    override fun int(minimum: Int, maximum: Int): KommandArgument<Int> {
+        return IntegerArgumentType.integer(minimum, maximum) to IntegerArgumentType::getInteger convert { it }
+    }
+
+    override fun float(minimum: Float, maximum: Float): KommandArgument<Float> {
+        return FloatArgumentType.floatArg(minimum, maximum) to FloatArgumentType::getFloat convert { it }
+    }
+
+    override fun double(minimum: Double, maximum: Double): KommandArgument<Double> {
+        return DoubleArgumentType.doubleArg(minimum, maximum) to DoubleArgumentType::getDouble convert { it }
+    }
+
+    override fun long(minimum: Long, maximum: Long): KommandArgument<Long> {
+        return LongArgumentType.longArg(minimum, maximum) to LongArgumentType::getLong convert { it }
+    }
+
+    override fun string(type: StringType): KommandArgument<String> {
+        return when (type) {
+            StringType.SINGLE_WORD -> StringArgumentType.word()
+            StringType.QOUTABLE_PHRASE -> StringArgumentType.string()
+            StringType.GREEDY_PHRASE -> StringArgumentType.greedyString()
+        } to StringArgumentType::getString convert { it }
     }
 }
