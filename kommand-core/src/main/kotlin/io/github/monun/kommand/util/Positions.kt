341b42b0676b249eb07d5625e9149283db8ba0ea
Patrick Choe <mailpatrickkr@gmail.com>
2021-07-07 21:38:17 +0900

    :zap: Positions.kt 확장함수 대신 클래스 안으로!

--- kommand-core/src/main/kotlin/io/github/monun/kommand/util/Positions.kt
+++ kommand-core/src/main/kotlin/io/github/monun/kommand/util/Positions.kt
@@ -4,42 +4,40 @@ import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.util.Vector
 
-data class Position(
-    val x: Double,
-    val y: Double,
-    val z: Double
-)
+data class Position(val x: Double, val y: Double, val z: Double) {
+    val asVector: Vector
+        get() = Vector(x, y, z)
 
-data class Position2D(
-    val x: Double,
-    val z: Double
-)
+    fun toLocation(world: World?, yaw: Float, pitch: Float): Location {
+        return Location(world, x, y, z, yaw, pitch)
+    }
 
-data class BlockPosition(
-    val x: Int,
-    val y: Int,
-    val z: Int
-)
+    fun toLocation(world: World?, rotation: Rotation): Location {
+        return Location(world, x, y, z, rotation.yaw, rotation.pitch)
+    }
+}
 
-data class BlockPosition2D(
-    val x: Int,
-    val z: Int
-)
+data class Position2D(val x: Double, val z: Double) {
+    fun withY(y: Double): Position {
+        return Position(x, y, z)
+    }
+}
 
-fun Position.toVector() = Vector(x, y, z)
+data class BlockPosition(val x: Int, val y: Int, val z: Int) {
+    val asVector: Vector
+        get() = Vector(x, y, z)
 
-fun Position.toLocation(world: World?, yaw: Float, pitch: Float) = Location(world, x, y, z, yaw, pitch)
+    fun toLocation(world: World?, yaw: Float, pitch: Float): Location {
+        return Location(world, x.toDouble(), y.toDouble(), z.toDouble(), yaw, pitch)
+    }
 
-fun Position.toLocation(world: World?, rotation: Rotation) = Location(world, x, y, z, rotation.yaw, rotation.pitch)
+    fun toLocation(world: World?, rotation: Rotation): Location {
+        return Location(world, x.toDouble(), y.toDouble(), z.toDouble(), rotation.yaw, rotation.pitch)
+    }
+}
 
-fun Position2D.to3D(y: Double) = Position(x, y, z)
-
-fun BlockPosition.toVector() = Vector(x, y, z)
-
-fun BlockPosition.toLocation(world: World?, yaw: Float, pitch: Float) =
-    Location(world, x.toDouble(), y.toDouble(), z.toDouble(), yaw, pitch)
-
-fun BlockPosition.toLocation(world: World?, rotation: Rotation) =
-    Location(world, x.toDouble(), y.toDouble(), z.toDouble(), rotation.yaw, rotation.pitch)
-
-fun BlockPosition2D.to3D(y: Int) = BlockPosition(x, y, z)
+data class BlockPosition2D(val x: Int, val z: Int) {
+    fun withY(y: Int): BlockPosition {
+        return BlockPosition(x, y, z)
+    }
+}
