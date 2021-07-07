3390ff4fd71fbdd22c095f855b3f8dc940b52199
Patrick Choe <mailpatrickkr@gmail.com>
2021-07-07 21:31:47 +0900

    :fire: Java 시러!

--- kommand-nms/v1_17_R1/src/main/kotlin/io/github/monun/kommand/v1_17_R1/NMSKommand.kt
+++ kommand-nms/v1_17_R1/src/main/kotlin/io/github/monun/kommand/v1_17_R1/NMSKommand.kt
@@ -1,13 +1,14 @@
 package io.github.monun.kommand.v1_17_R1
 
+import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.builder.ArgumentBuilder
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import io.github.monun.kommand.Kommand
+import io.github.monun.kommand.internal.AbstractKommandNode
 import io.github.monun.kommand.internal.ArgumentNodeImpl
 import io.github.monun.kommand.internal.KommandDispatcherImpl
-import io.github.monun.kommand.internal.AbstractKommandNode
 import io.github.monun.kommand.internal.LiteralNodeImpl
-import io.github.monun.kommand.v1_17_R1.internal.ArgumentSupport
 import io.github.monun.kommand.v1_17_R1.internal.NMSKommandContext
 import io.github.monun.kommand.v1_17_R1.internal.NMSKommandSource
 import net.minecraft.commands.CommandSourceStack
@@ -18,17 +19,17 @@ class NMSKommand : Kommand {
     override fun register(dispatcher: KommandDispatcherImpl, aliases: List<String>) {
         val nms = (Bukkit.getServer() as CraftServer).server.commands.dispatcher
         val node = nms.register(dispatcher.root.convert() as LiteralArgumentBuilder<CommandSourceStack>)
-        aliases.forEach { nms.register(ArgumentSupport.literal(it).redirect(node)) }
+        aliases.forEach { nms.register(literal(it).redirect(node)) }
     }
 }
 
 private fun AbstractKommandNode.convert(): ArgumentBuilder<CommandSourceStack, *> {
     return when (this) {
-        is LiteralNodeImpl -> ArgumentSupport.literal(name)
+        is LiteralNodeImpl -> literal(name)
         is ArgumentNodeImpl -> {
             val kommandArgument = argument as NMSKommandArgument<*>
             val type = kommandArgument.type
-            ArgumentSupport.argument(name, type).apply {
+            argument(name, type).apply {
                 kommandArgument.suggestionProvider?.let { provider ->
                     suggests { context, builder ->
                         val suggestion = NMSKommandSuggestion(builder)
@@ -64,4 +65,12 @@ private fun AbstractKommandNode.convert(): ArgumentBuilder<CommandSourceStack, *
             then(node.convert())
         }
     }
+}
+
+private fun literal(name: String): LiteralArgumentBuilder<CommandSourceStack> {
+    return LiteralArgumentBuilder.literal(name)
+}
+
+private fun argument(name: String, argumentType: ArgumentType<*>): RequiredArgumentBuilder<CommandSourceStack, *> {
+    return RequiredArgumentBuilder.argument(name, argumentType)
 }
