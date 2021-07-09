aced91eeb424fee5a4f694b5e959f72d8081fa9f
Monun <monun1010@gmail.com>
2021-07-10 04:06:30 +0900

    :wrench: 작업공간 설정을 sh 대신 gradle task 이용
    크로스 플랫폼
    gradle.properties 파일에 java 경로 필요
    java8=.../java.exe
    java16=.../java.exe

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.FileFilter
+
 plugins {
     kotlin("jvm") version "1.5.20"
 }
@@ -39,5 +41,65 @@ subprojects {
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
         testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
         testImplementation("org.mockito:mockito-core:3.6.28")
+    }
+}
+
+tasks {
+    create<DefaultTask>("setupWorkspace") {
+        // File: gradle.properties
+        // java8="C:/Program Files/Zulu/zulu-8/bin/java.exe"
+        // java16="C:/Program Files/Zulu/zulu-16/bin/java.exe"
+
+        val buildToolsDir = File(rootDir, ".buildtools").also { it.mkdirs() }
+        val buildToolsJar = File(buildToolsDir, "BuildTools.jar")
+        val memory = "1G"
+        val versions = linkedSetOf(
+            "1.17"
+        )
+
+        val home = System.getProperty("user.home")
+        val spigot = "spigot"
+        val mavenLocal = File("$home/.m2/repository/org/spigotmc/$spigot")
+        val repos = mavenLocal.listFiles(FileFilter { it.isDirectory }) ?: emptyArray()
+        versions.removeIf { version ->
+            repos.find { it.name.startsWith("$version-R") }?.let { repo ->
+                val artifactName = "$spigot-${repo.name}"
+                val jar = File(repo, "$artifactName.jar")
+                val pom = File(repo, "$artifactName.pom")
+
+                return@removeIf (jar.exists() && pom.exists()).also {
+                    if (it) println("Skip download version $version")
+                }
+            }
+            false
+        }
+
+        val download by registering(de.undercouch.gradle.tasks.download.Download::class) {
+            src("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
+            dest(buildToolsJar)
+            onlyIfModified(true)
+        }
+        download.get().download()
+
+        fun java(name: String): String {
+            return project.properties[name]?.toString()
+                ?: throw NullPointerException("Please define $name in gradle.properties.")
+        }
+
+        versions.forEach { version ->
+            logger.info("Download version $version")
+            val java = if (version < "1.17") java("java8") else java("java16")
+            logger.info("Use java $java")
+
+            runCatching {
+                exec {
+                    workingDir(buildToolsDir)
+                    commandLine(java, "-Xmx$memory", "-jar", buildToolsJar.name, "--rev", version, "--remapped")
+                }
+            }.onFailure {
+                logger.warn("Failed to download version $version")
+                throw it
+            }
+        }
     }
 }
