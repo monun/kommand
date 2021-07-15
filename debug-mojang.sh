cbe981e1bc4a0edda9f673173d15f4717a771dcb
Monun <monun1010@gmail.com>
2021-07-15 17:19:09 +0900

    :hammer: 서버스크립트 업데이트

--- debug-mojang.sh
+++ debug-mojang.sh
@@ -2,6 +2,7 @@
 
 ./gradlew clean debugMojangJar
 
+server=$HOME/.m2/repository/io/papermc/paper/paper/1.17.1-R0.1-SNAPSHOT/paper-1.17.1-R0.1-SNAPSHOT-mojang-mapped.jar
 plugins=(
 )
 
@@ -9,14 +10,14 @@ script=$(basename "$0")
 server_folder=".${script%.*}"
 mkdir -p "$server_folder"
 
-server_script="server.sh"
-server_config="server.sh.conf"
+server_script="start.sh"
+server_config="$server_script.conf"
 
 if [ ! -f "$server_folder/$server_script" ]; then
   if [ -f ".server/$server_script" ]; then
     cp ".server/$server_script" "$server_folder/$server_script"
   else
-    curl 'https://raw.githubusercontent.com/monun/server-script/master/.server/server.sh' > "$server_folder/server.sh"
+    wget -qc -P "$server_folder" -N "https://raw.githubusercontent.com/monun/server-script/master/.server/$server_script"
   fi
 fi
 
@@ -24,7 +25,7 @@ cd "$server_folder" || exit
 
 if [ ! -f "$server_config" ]; then
     cat << EOF > $server_config
-server=$HOME/.m2/repository/io/papermc/paper/paper/1.17.1-R0.1-SNAPSHOT/paper-1.17.1-R0.1-SNAPSHOT-mojang-mapped.jar
+server=$server
 debug=true
 debug_port=5005
 backup=false
@@ -34,7 +35,7 @@ plugins=(
 EOF
     for plugin in "${plugins[@]}"
     do
-        echo "  \"$plugin\"" >> $server_config
+        echo "  \"$plugin\""   >> $server_config
     done
     echo ")" >> $server_config
 fi
